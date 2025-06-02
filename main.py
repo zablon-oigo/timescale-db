8e6989b2a880cdc20a942c9fa52c55c4e1b749c0
Zablon <143833326+zablon-oigo@users.noreply.github.com>
2025-06-02 23:37:23 +0300

    add list of stock

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-def on_event(event):
-    print(event)
+import time
+   from twelvedata import TDClient
+
+    messages_history = []
+
+    def on_event(event):
+     print(event)
+     messages_history.append(event)
