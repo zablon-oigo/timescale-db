f12ad1a12e26ec06dc4d6e2bf5317881c69be5c0
Zablon <143833326+zablon-oigo@users.noreply.github.com>
2025-06-03 23:07:57 +0300

    add twelve data

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import time
-   from twelvedata import TDClient
+
+from twelvedata import TDClient
 
     messages_history = []
 
     def on_event(event):
      print(event)
      messages_history.append(event)
+   td = TDClient(apikey=os.getenv())
+   ws = td.websocket(symbols=["BTC/USD", "ETH/USD"], on_event=on_event)
+   ws.subscribe(['ETH/BTC', 'AAPL'])
+   ws.connect()
