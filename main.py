90436f2594a07747d6e0536a2973ae1fb53ac3b4
zablon <zablonoigo51@gmail.com>
2025-06-05 18:26:51 +0300

    pass test key

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import time
+from dotenv import load_dotenv 
+import os 
+
+load_dotenv()
 
 from twelvedata import TDClient
 
-    messages_history = []
+messages_history = []
 
-    def on_event(event):
-     print(event)
-     messages_history.append(event)
-   td = TDClient(apikey=os.getenv())
-   ws = td.websocket(symbols=["BTC/USD", "ETH/USD"], on_event=on_event)
-   ws.subscribe(['ETH/BTC', 'AAPL'])
-   ws.connect()
+def on_event(event):
+   print(event)
+   messages_history.append(event)
+td = TDClient(apikey=os.getenv("API_KEY"))
+ws = td.websocket(symbols=["BTC/USD", "ETH/USD"], on_event=on_event)
+ws.subscribe(['ETH/BTC', 'AAPL'])
+ws.connect()
 while True:
    print('messages received: ', len(messages_history))
    ws.heartbeat()
