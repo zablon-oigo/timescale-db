bd7a3838b8d1c56b5be9fab652958cab9ba9bafa
Zablon <143833326+zablon-oigo@users.noreply.github.com>
2025-06-07 08:14:15 +0300

    websocket event

--- main.py
+++ main.py
@@ -23,3 +23,8 @@ while True:
    print('messages received: ', len(messages_history))
    ws.heartbeat()
    time.sleep(10)
+def start(self, symbols):
+        td = TDClient(apikey=os.getenv("API_KEY"))
+        ws = td.websocket(on_event=self._on_event)
+        ws.subscribe(symbols)
+        ws.connect()
