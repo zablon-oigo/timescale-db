8af0b7120dd00b33c6841bdd8b898f38a98d9728
Zablon <143833326+zablon-oigo@users.noreply.github.com>
2025-06-11 21:53:57 +0300

    add pytest fixture

--- tests.py
+++ tests.py
@@ -1,3 +1,10 @@
 import pytest
 from unittest.mock import MagicMock
 from datetime import datetime, timezone
+
+@pytest.fixture
+def mock_conn():
+    conn = MagicMock()
+    cursor = MagicMock()
+    conn.cursor.return_value = cursor
+    return conn
